0f7e1d0a4e6fb2cd14ca303fbb1d2b9a1e44cb11
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-03-24 16:28:16 +0100

    ReadFiles påbegyndt

--- 06_del2/WCU/functionality/ReadFiles.java
+++ 06_del2/WCU/functionality/ReadFiles.java
@@ -1,5 +1,45 @@
 package functionality;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ReadFiles {
+
+	private File log;
+	private File store;
+
+	public ReadFiles(){
+		log = new File("/files/Log.txt");
+		store = new File("/files.txt/store.txt");
+	}
+
+	public String getProductName(int productNumber) throws FileNotFoundException{
+		// Read store.txt, find produktnummeret og returner produktnavnet
+		Scanner scan = null;
+		try {
+			scan = new Scanner(store);
+			int p =0;
+			do {
+				p = scan.nextInt();
+			} while (p != productNumber);
+		} finally {
+			scan.close();
+		}
+		return null;
+	}
+
+	public void writeLog(String line){
+		// tilføj en linje til Log.txt
+	}
+
+	public void updProductInventory(int productNumber, double amountUsed){
+		// Ret i inventory i store.txt
+	}
+
+	public double getProductInventory(int productNumber){
+		// find mængde på lager for det pågældende produkt
+		return 0;
+	}
 
 }
