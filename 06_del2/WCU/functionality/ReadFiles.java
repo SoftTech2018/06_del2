65a28528b9e10c713d0324a077dde367dd6bf4e5
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-04-02 13:33:50 +0200

    Tilføjet funktionalitet til ReadFiles.

--- 06_del2/WCU/functionality/ReadFiles.java
+++ 06_del2/WCU/functionality/ReadFiles.java
@@ -1,32 +1,43 @@
 package functionality;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReadFiles {
 
 	private File log;
 	private File store;
-	private int logLines;
+	private int storeLines;
 
 	public ReadFiles() throws FileNotFoundException{
-		log = new File("/files/Log.txt");
-		store = new File("/files.txt/store.txt");
-		logLines = countLines();
+		log = new File("files/Log.txt");
+		store = new File("files/store.txt");
+		storeLines = countLines();
+		if (storeLines != 4) // Hvor mange linjer varer skal vi have?
+			throw new FileNotFoundException();
 	}
 
 	private int countLines() throws FileNotFoundException{
-		Scanner scan = null;
 		int i = 0;
-		try {
-			scan = new Scanner(store);
-			while (scan.hasNextLine()){
+		try (BufferedReader br = new BufferedReader(new FileReader(store));) {
+			while (br.readLine() != null){
 				i++;
 			}
-		} finally {
-			scan.close();
-		}
+		} catch (IOException e) {
+			throw new FileNotFoundException();
+		} 
 		return i;
 	}
 
@@ -52,17 +63,65 @@ public class ReadFiles {
 		return out;
 	}
 
-	public void writeLog(String line){
+	public void writeLog(int oprNr, int vareNr, double afvejet, double lager) throws FileNotFoundException{
 		// tilføj en linje til Log.txt
+		//Dato, Tid, Operatørnummer, varenummer, afvejning (i kg), tilbage på lager (i kg)
+		//I denne fil ændres eksisterende data aldrig – der ”appendes” altid. 
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(log, true)));){
+			java.util.Date date = new java.util.Date();						
+			out.println(new Timestamp(date.getTime())+","+oprNr+","+vareNr+","+afvejet+","+lager);
+		}catch (IOException e) {
+			throw new FileNotFoundException();
+		}
 	}
 
-	public void updProductInventory(int productNumber, double amountUsed){
+	public boolean updProductInventory(int productNumber, double amountUsed) throws FileNotFoundException{
 		// Ret i inventory i store.txt
+		boolean updated = false;
+		String[] data = readFile(store);
+		int p = 0;
+		while (p < data.length){
+			String[] line = data[p].split(",");
+			if (productNumber == Integer.parseInt(line[0])){
+				data[p] = data[p].replaceAll(line[2], Double.toString(Double.parseDouble(line[2])-amountUsed));
+				updated = true;
+			} 
+			p++;
+		}
+		if (updated)
+			writeFile(store, data);
+		return updated;
 	}
 
 	public double getProductInventory(int productNumber){
 		// find mængde på lager for det pågældende produkt
 		return 0;
+	}
+
+	private String[] readFile(File fil) throws FileNotFoundException{
+		List<String> data = new ArrayList<String>();
+		String linje = null;
+		try (BufferedReader br = new BufferedReader(new FileReader(store));){
+			while ((linje = br.readLine()) != null){
+				data.add(linje);
+			}
+		} catch (IOException e) {
+			e.printStackTrace(); // Fjernes i endeligt program
+			throw new FileNotFoundException();
+		}
+		return data.toArray(new String[data.size()]);
+	}
+
+	private void writeFile(File fil, String[] data) throws FileNotFoundException{
+		try (BufferedWriter write = new BufferedWriter(new FileWriter(fil));){
+			for (String s : data){
+				write.write(s);
+				write.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace(); // Fjernes i endelig kode
+			throw new FileNotFoundException();
+		}
 	}
 
 }
--- 06_del2/WCU/wcuMain/Main.java
+++ 06_del2/WCU/wcuMain/Main.java
@@ -1,10 +1,25 @@
 package wcuMain;
 
+import java.io.FileNotFoundException;
+
+import functionality.ReadFiles;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ReadFiles rf = new ReadFiles();
+			System.out.println("Test af ReadFiles.getProductName (produktnr 10): " + rf.getProductName(10));
+			System.out.println("Test af ReadFiles.getProductName (produktnr 0): " + rf.getProductName(0));
+			System.out.println("Skriver til log - OprNr 10, VareNr 1234, afvejet 1.478, lagerbeholdning 0.719");
+			rf.writeLog(10, 1234, 1.478, 0.719);
+			System.out.println("Opdaterer text-filen store.txt for produktnummer 10: " + rf.updProductInventory(10, 2.14));
 
+		} catch (FileNotFoundException e) {
+			System.out.println("Noget i filbehandlingen gik grueligt galt :-( Kontakt udvikleren.");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
