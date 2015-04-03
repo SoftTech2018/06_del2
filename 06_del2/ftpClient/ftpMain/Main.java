5365242190207be7d0cedd990f9fa721cd3417f1
Ebbe <Ebbe@10.16.170.76>
2015-04-03 10:30:54 +0200

    Tilføjelse: Tilføjet showMenu, list og retrieve metoder

--- 06_del2/ftpClient/ftpMain/Main.java
+++ 06_del2/ftpClient/ftpMain/Main.java
@@ -1,10 +1,14 @@
 package ftpMain;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner menuScan = new Scanner(System.in);
+		
+		IMenu menu = new Menu(menuScan);
 	}
 
 }
--- 06_del2/ftpClient/ftpMain/Menu.java
+++ 06_del2/ftpClient/ftpMain/Menu.java
@@ -1,5 +1,33 @@
 package ftpMain;
 
+import java.util.Scanner;
+
 public class Menu implements IMenu {
 
+	private Scanner menuScan;
+	
+	public Menu(Scanner menuScan) {
+		this.menuScan = menuScan;
+	}
+	
+	public void showMenu() {
+		System.out.println("Indtast 1 for list-kommando");
+		System.out.println("Indtast 2 for retrieve-kommando");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+	
+	public void list(String list) { //String list returnerer listen af filer på FTP-serveren, omdannet til strings
+		System.out.println("Du har valgt list-kommandoen, du har nu følgende muligheder: ");
+		System.out.println(list); //Her skal der laves en for løkke der printer alle filtitlerne ud
+		System.out.println("Indtast 2 for retrieve, efterfulgt af tallet ud fra den fil du vil hente.");
+	}
+	
+	public void retrieve(File file) { //Hvilken type skal filen være??
+		System.out.println("Du har hentet fil " + file);
+	}
+	
 }
