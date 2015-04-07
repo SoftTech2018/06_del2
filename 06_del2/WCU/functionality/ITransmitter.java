60dd1694778ec24f24d76d33f42dfcebac64ea18
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-04-07 15:33:38 +0200

    Udkast til transmitter til WCU lavet.

--- 06_del2/WCU/functionality/ITransmitter.java
+++ 06_del2/WCU/functionality/ITransmitter.java
@@ -1,5 +1,9 @@
 package functionality;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public interface ITransmitter {
 
 	/**
@@ -8,30 +12,36 @@ public interface ITransmitter {
 	 * @param txt2
 	 * @param txt3
 	 * @return Svaret fra operatøren
+	 * @throws IOException 
 	 */
-	public abstract String RM20(String txt1, String txt2, String txt3);
+	public abstract String RM20(String txt1, String txt2, String txt3) throws IOException;
 
 	/**
 	 * Viser en tekst på vægten
 	 * @param txt
 	 * @return Returbesked fra vægt (ES = fejl)
+	 * @throws IOException 
 	 */
-	public abstract boolean P111(String txt);
+	public abstract boolean P111(String txt) throws IOException;
 
 	/**
 	 * Beder om aflæsning af vægten
 	 * @return Den aflæste vægt
+	 * @throws IOException 
 	 */
-	public abstract String S();
+	public abstract String S() throws IOException;
 
 	/**
 	 * Tarer vægten
 	 * @return
+	 * @throws IOException 
 	 */
-	public abstract String T();
+	public abstract String T() throws IOException;
 
-	public abstract boolean D();
+	public abstract boolean D(String txt) throws IOException;
 
-	public abstract boolean DW();
+	public abstract boolean DW() throws IOException;
+
+	void connected(BufferedReader in, PrintWriter out);
 
 }
