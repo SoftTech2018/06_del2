344dcfea687664acb2c4496ba98fbbdf9ed3f7fa
JacobWorckJepsen <JacobWorckJepsen@10.16.84.169>
2015-04-07 19:30:56 +0200

    Rod

--- 06_del2/ftpClient/ftpMain/FTPclient.java
+++ 06_del2/ftpClient/ftpMain/FTPclient.java
@@ -1,48 +1,82 @@
 package ftpMain;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-import org.apache.commons.net.ftp.FTP;
-import org.apache.commons.net.ftp.FTPClient;
-
-public class FTPclient extends FTP{
+public class FTPclient{
 	
-	String server = "www.facebook.com";
+	String server = "speedtest.tele2.net";
 	int port = 21;
-	String user = "user";
-	String pass = "password";
 	
-	
-	FTPClient ftp = new FTPClient();
-	
-	public void spasser(){
+	public FTPclient(){
 		
+	}
+	
+	public void command(){
 		try {
-			ftp.connect(server, port);
-			ftp.login(user, pass);
-			ftp.enterLocalPassiveMode();
-			ftp.setFileType(BINARY_FILE_TYPE);
+			Socket socket21 = new Socket("speedtest.tele2.net", 21);
+			PrintWriter out = new PrintWriter(socket21.getOutputStream(),true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket21.getInputStream()));
 			
-			String remoteFile1 = "/test/video.mp4";
-			File downloadFile1 = new File("D:/Downloads/video.mp4");
-			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
-			boolean success = ftp.retrieveFile(remoteFile1, outputStream1);
-			outputStream1.close();
 			
-			if (success) {
-				System.out.println("File #1 has been downloaded successfully.");
-			}
-			
-		} catch (IOException e){
-			System.out.println("Error: " + e.getMessage());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} finally {
-			System.out.println("Sup");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
+	
+	public void data(){
+		try {
+			Socket socket20 = new Socket("speedtest.tele2.net", 20);
+			PrintWriter out = new PrintWriter(socket20.getOutputStream(),true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket20.getInputStream()));
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+//	public void dataCommand(int port, String server){
+//		spasser(localHost, 21);
+//		if (true)
+//			spasser(localHost, 20);
+//	}
+	
+//	public void spasser(String server, int port){
+//		
+//		try {
+//			ftp.connect(server, port);
+//			ftp.login(user, pass);
+//			ftp.enterLocalPassiveMode();
+//			ftp.setFileType(BINARY_FILE_TYPE);
+//			
+//			String remoteFile1 = "/test/video.mp4";
+//			File downloadFile1 = new File("D:/Downloads/video.mp4");
+//			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
+//			boolean success = ftp.retrieveFile(remoteFile1, outputStream1);
+//			outputStream1.close();
+//			
+//			if (success) {
+//				System.out.println("File #1 has been downloaded successfully.");
+//			}
+//			
+//		} catch (IOException e){
+//			System.out.println("Error: " + e.getMessage());
+//			e.printStackTrace();
+//		} finally {
+//			System.out.println("Sup");
+//		}
+//	}
 	
 }
