af772d462e14c29a8369d033ed3e2809390df187
JACOB <JACOB@JACOB-Pc>
2015-04-08 18:17:44 +0200

    Tilføjelse: FTP klient - mangler funktionalitet

--- 06_del2/ftpClient/ftpMain/FTPclient.java
+++ 06_del2/ftpClient/ftpMain/FTPclient.java
@@ -1,82 +1,77 @@
 package ftpMain;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class FTPclient{
 	
-	String server = "speedtest.tele2.net";
-	int port = 21;
+	private Socket socket = null;
+	private BufferedReader reader = null;
+	private BufferedWriter writer = null;
+	private static boolean DEBUG = false;
 	
 	public FTPclient(){
 		
 	}
 	
-	public void command(){
+	public synchronized void connect(String host, int port, String user, String pass) throws IOException{
+		if (socket != null){
+			throw new IOException("FTP klienten er allerede forbundet til FTP-serveren");
+		}
+		
+		socket = new Socket(host, port);
+		reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		
+		String response = readLine();
+		if (!response.startsWith("220 ")){ //220 = Service ready for new user
+			throw new IOException("FTP klienten modtog ukendt respons ved oprettelse af forbindelse til server: "+response);
+		}
+		
+		sendLine("USER " + user);
+		
+		response = readLine();
+		if (!response.startsWith("331 ")){ //331 = Brugernavn OK, venter på Password
+			throw new IOException("FTP klienten modtog forkert respons fra server efter brugernavn blev indtastet: "+ response);
+		}
+		
+		sendLine("PASS " + pass);
+		
+		response = readLine();
+		if (!response.startsWith("230-User ")){
+			throw new IOException("FTP klienten nægtet adgang med det givne password: "+ response);
+		}
+		
+	}
+	
+	private void sendLine(String line) throws IOException {
+		if (socket == null) {
+			throw new IOException("FTP klienten har ingen forbindelse til serveren.");
+		}
 		try {
-			Socket socket21 = new Socket("speedtest.tele2.net", 21);
-			PrintWriter out = new PrintWriter(socket21.getOutputStream(),true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket21.getInputStream()));
-			
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			writer.write(line + "\r\n");
+			writer.flush();
+			if (DEBUG) {
+				System.out.println("> " + line);
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			socket = null;
+			throw e;
 		}
 	}
 	
-	public void data(){
-		try {
-			Socket socket20 = new Socket("speedtest.tele2.net", 20);
-			PrintWriter out = new PrintWriter(socket20.getOutputStream(),true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket20.getInputStream()));
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private String readLine() throws IOException {
+		String line = reader.readLine();
+		if (DEBUG) {
+			System.out.println("< " + line);
 		}
+		return line;
 	}
-	
-//	public void dataCommand(int port, String server){
-//		spasser(localHost, 21);
-//		if (true)
-//			spasser(localHost, 20);
-//	}
-	
-//	public void spasser(String server, int port){
-//		
-//		try {
-//			ftp.connect(server, port);
-//			ftp.login(user, pass);
-//			ftp.enterLocalPassiveMode();
-//			ftp.setFileType(BINARY_FILE_TYPE);
-//			
-//			String remoteFile1 = "/test/video.mp4";
-//			File downloadFile1 = new File("D:/Downloads/video.mp4");
-//			OutputStream outputStream1 = new BufferedOutputStream(new FileOutputStream(downloadFile1));
-//			boolean success = ftp.retrieveFile(remoteFile1, outputStream1);
-//			outputStream1.close();
-//			
-//			if (success) {
-//				System.out.println("File #1 has been downloaded successfully.");
-//			}
-//			
-//		} catch (IOException e){
-//			System.out.println("Error: " + e.getMessage());
-//			e.printStackTrace();
-//		} finally {
-//			System.out.println("Sup");
-//		}
-//	}
 	
 }
