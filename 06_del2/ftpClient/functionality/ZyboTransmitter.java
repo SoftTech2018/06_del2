74e29b03d0b14ba8514b92e75a0aa63930070450
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-04-08 10:33:51 +0200

    Mere transmitter

--- 06_del2/ftpClient/functionality/ZyboTransmitter.java
+++ 06_del2/ftpClient/functionality/ZyboTransmitter.java
@@ -6,6 +6,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ZyboTransmitter {
 	private PrintWriter out;
@@ -16,6 +18,14 @@ public class ZyboTransmitter {
 		this.in = in;
 	}
 	
+	/**
+	 * Sender kommando til Zybo-board for en specifik sensor
+	 * @param command S = Start måling, B = Stop måling, T = Returner måling, I = Sæt samplingsinterval
+	 * @param sensor Nummeret på sensoren der sendes kommando til
+	 * @param parameter Kan være null - skal kun benyttes hvis samplingsinterval skal ændres
+	 * @return Besked fra Zyboboardet
+	 * @throws IOException
+	 */
 	public String sendCommand(String command, int sensor, String parameter) throws IOException{
 		String message = command + " " + sensor;
 		String reply = null;
@@ -36,6 +46,8 @@ public class ZyboTransmitter {
 		}
 		break;
 		case "I":{ // Sæt samplingsinterval
+			if (parameter == null)
+				parameter = "12000";
 			out.println(message + " " + parameter); // F.eks. "I 222 12000" betyder at sensor 222 skal ændre samplingsinterval til 120000
 			reply = in.readLine();
 		}
@@ -46,6 +58,21 @@ public class ZyboTransmitter {
 		}
 		
 		return reply;
+	}
+	
+	/**
+	 * Beder Zybo-boardet om en liste af aktive sensorer
+	 * @return Liste af aktive sensorer
+	 * @throws IOException
+	 */
+	public List<String> getSensors() throws IOException{
+		out.println("LIST");
+		String sensor = null;
+		List<String> sensorList = new ArrayList<String>();
+		while (!(sensor = in.readLine()).equalsIgnoreCase("END")){ // Antager at Zybo sender "END" når listen er færdig
+			sensorList.add(sensor);
+		}
+		return sensorList;
 	}
 
 }
